6babaabcc21c2ab4807bdb7596110bdd153d68e3
Arthur Ligocki <artwligocki@gmail.com>
2023-06-01 16:52:39 -0300

    seed revamp and game banners added along with default games

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,22 @@ require 'faker'
 
 puts "Initializing seed..."
 
+Order.destroy_all
 Product.destroy_all
 Game.destroy_all
 User.destroy_all
 
-games = ['CS:GO', 'Dota 2', 'Team Fortress 2', 'Rust',
-         'Final Fantasy XIV', 'Unturned', 'Rainbow Six Siege', 'Minecraft']
-
 puts "Previous instances destroyed."
+
+games = [['CS:GO', 'app/assets/images/game-posters/csgo.jpg'], ['Dota 2', 'app/assets/images/game-posters/dota-2.jpg'], ['Team Fortress 2', 'app/assets/images/game-posters/tf2.jpg'], ['Rust', 'app/assets/images/game-posters/rust.webp'],
+         ['Final Fantasy XIV', 'app/assets/images/game-posters/final-fantasy-xiv.png'], ['Unturned', 'app/assets/images/game-posters/Unturned.webp'], ['Rainbow Six Siege', 'app/assets/images/game-posters/r6.jpg'], ['Minecraft', 'app/assets/images/game-posters/minecraft.jpg']]
 
 puts "Seeding..."
 games.each do |game|
-  Game.create(title: game)
-  puts Game.last.title
+  g = Game.new(title: game[0])
+  g.photo.attach(io: File.open(game[1]), filename: game[0].downcase + '.jpg')
+  g.save!
+  puts g.title
 end
 
 10.times do
@@ -25,6 +28,7 @@ end
     email: Faker::Internet.email,
     password: "123123"
   )
+  puts "User: #{User.last.first_name}"
   3.times do
     Product.create(
       name: Faker::Cannabis.strain,
@@ -33,9 +37,11 @@ end
       user_id: User.last.id,
       game_id: Game.all.sample.id
     )
-    puts Product.last.name
+    puts "   Product: #{Product.last.name}"
   end
-  puts User.last.first_name
 end
+
+User.create(first_name: "Admin", last_name: "Admin", trade_url: "https://steamcommunity.com/tradeoffer/new/?partner=123456789&token=123456789", email: "admin@admin", password: "123123")
+puts "Created admin: #{User.last.email}"
 
 puts "Seeding complete!"
